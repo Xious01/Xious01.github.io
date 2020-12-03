bffee2293f81d671801d65ef4a3c38787ca7945b
Xious01 <36866305+Xious01@users.noreply.github.com>
2020-12-03 15:00:49 -0600

    Add files via upload

---
+++ The start of my C#.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoodbyeWorldApplication; {
+    class GoodbyeWorld{
+        static void Main(string[] args) {
+            /* I hate the world */
+            Console.Writeline("GoodBye World");
+            Console.ReadKey();
+        }
+    }
+}
